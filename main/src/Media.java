bca7137cbcc87f4bc36adc193f9b9de0fa4875c8
Edgar1081 <luisedgarflores@ciencias.unam.mx>
2021-10-19 18:03:59 -0500

    remove prints

--- main/src/Media.java
+++ main/src/Media.java
@@ -9,7 +9,6 @@ public class Media{
         for(double i : datos)
             s += i;
         double n = datos.length;
-        System.out.println("Media ARITMETICA\n" + s/n);
         return s/n;
     }
 
@@ -19,7 +18,6 @@ public class Media{
             s += 1.0/i;
   
         double n = (double) datos.length;
-        //System.out.println("Media ARMONICA\n" + n/s);
         return n/s;
 
     }
@@ -31,7 +29,6 @@ public class Media{
         }
         double n = (double) datos.length;
         double r = Math.pow(p, 1.0/n);   
-        //System.out.println("Media GEOMETRICA\n" + r);
         return r;
     }
 
@@ -46,7 +43,6 @@ public class Media{
         double n = 0;
         for(int i = 0; i < datos.length; i++)
             n += datos[i] * pesos[i];
-        //System.out.println("Media PONDERADA\n" + n/d);
         return n/d;
     }
 }
